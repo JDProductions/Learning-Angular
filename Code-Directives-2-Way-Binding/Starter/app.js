5a46b95bee4a8ce4d9c4e012ae6e43580ef9aee3
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-03-30 15:48:48 -0400

    sdfsd

--- Code-Directives-2-Way-Binding/Starter/app.js
+++ Code-Directives-2-Way-Binding/Starter/app.js
@@ -1,9 +1,26 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$log', function($scope, $filter, $log) {
     
     $scope.handle = '';
     $scope.lowercasehandle = function() {                               
         return $filter('lowercase')($scope.handle);
     };
+    
+    $scope.$watch('handle' ,function(newValue, oldValue){
+        $log.info('Changed!');
+        $log.log('Old' + oldValue);
+        $log.log('New' + newValue);
+    });
+    
+    // This is a function outside of angular js
+    // To make this easier you could just add the timeout dependency
+    setTimeout(function(){
+        // How to let angular know to start a digest cycle
+        $scope.$apply(function()  {
+        $scope.handle = 'newtwitterhandle';
+        console.log('Scope changed!');
+            }); // end digest cycle
+    } ,3000);
+
 }]);
